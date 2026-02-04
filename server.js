1cc9556fb6173ef8905ce0b3ae27db2e4fb0f50a
Sanika M. Bhore <sanikambhore21@gmail.com>
2026-02-04 12:58:08 +0530

    t

--- server.js
+++ server.js
@@ -48,8 +48,8 @@ app.use((err, req, res, next) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server running on ${PORT}`);
+app.listen( "0.0.0.0", () => {
+  console.log(`Server running`);
 });
 
 module.exports = app;
